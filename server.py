d6c586b2c79e4483574fed9c8c5a9c8316e5714c
lrq <lrq1988@gmail.com>
2014-03-27 14:52:22 +0800

    branch test

--- server.py
+++ server.py
@@ -13,4 +13,5 @@ if __name__ == "__main__":
     application.listen(int(PORT))
     print('Server is running at http://127.0.0.1:%s/' % PORT)
     print('Quit the server with CONTROL-C')
+    print('Quit the server with CONTROL-C')
     tornado.ioloop.IOLoop.instance().start()
